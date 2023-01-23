c6f5954ed65418b165f341e076e59b914401ae9d
Pusp203 <puspjoc98@gmail.com>
2023-01-23 15:47:49 +0545

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,75 +1,63 @@
 import React from "react";
 import "./footer.scss";
 import Arshakir from "../../assets/Arshakir.png";
-// import { BsTwitter } from "react-icons/Bs";
+import { BsTwitter } from "react-icons/Bs";
 
 const Footer = () => {
   return (
-    // <div className="container ">
-    <div class=" footer-maincontainer  ">
-      <div className="footer__subcontainer flex">
-        <div class="companies__description flex ">
-          <a href="#" class="sass-logo">
-            <img src={Arshakir} alt="" />
-          </a>
-          <p>Finance helps companies manage payments easily.</p>
-          {/* 
-          <div className="socialmedia__icons flex">
-            <a href="">
-              <BsTwitter />
-            </a>
-            <a href="">
-              <BsTwitter />
-            </a>
-            <a href="">
-              <BsTwitter />
-            </a>
-            <a href="">
-              <BsTwitter />
-            </a>
-          </div> */}
-        </div>
-        <div className="nav__itemswrap flex">
-          <div class="link-column flex ">
-            <h4>Company</h4>
-            <p>About Us</p>
-            <p>Careers</p>
-            <p>Blog</p>
-            <p>Pricing</p>
-          </div>
-          <div class="link-column flex">
-            <h4>Resources</h4>
-            <p>Proposal Template</p>
-            <p>Invoice Template</p>
-            <p>Tuturoial</p>
-            <p>How to write a contract</p>
-          </div>
-          <div class="link-column flex">
-            <h4>Join Our Newsletter</h4>
+    <div className=" footer-maincontainer  ">
+      <div className="footer__subcontainer ">
+        <div className="footer__subcontainer--companiesdetails  ">
+          <img src={Arshakir} alt="" />
 
-            <div class="email__1 flex">
-              <input
-                class="input"
-                type="text"
-                name="address"
-                id="address"
-                placeholder="Your email address"
-              />
-              <button class="blue__btn">Subscribe</button>
-            </div>
-            <div className="news__letter">
-              <p>
-                * Wil send you weekly updates for your better finance
-                management.
-              </p>
-            </div>
+          <p>Finance helps companies manage payments easily.</p>
+
+          <div className="footer__subcontainer--companiesdetails--socialmedialinks">
+            <BsTwitter />
+            <BsTwitter />
+            <BsTwitter />
+            <BsTwitter />
+          </div>
+        </div>
+
+        <div className="footer__subcontainer--navwrap ">
+          <h4 className="nav__wrap-heading">Company</h4>
+          <p>About Us</p>
+          <p>Careers</p>
+          <p>Blog</p>
+          <p>Pricing</p>
+        </div>
+        <div className="footer__subcontainer--navwrap ">
+          <h4 className="nav__wrap-heading">Resources</h4>
+          <p>Proposal Template</p>
+          <p>Invoice Template</p>
+          <p>Tuturoial</p>
+          <p>How to write a contract</p>
+        </div>
+        <div className="footer__subcontainer--newslettersection ">
+          <h4 className="nav__wrap-heading">Join Our Newsletter</h4>
+
+          <div className="email__1">
+            <input
+              className="input"
+              type="text"
+              name="address"
+              id="address"
+              placeholder="Your email address"
+            />
+            <button className="blue__btn">Subscribe</button>
+          </div>
+          <div>
+            <p className="mandatory__field">
+              * Wil send you weekly updates for your better finance management.
+            </p>
           </div>
         </div>
       </div>
       <hr />
       <div className="sub__footer">
         {" "}
-        <p>Copyright @ AR Shakir 2022. All Rights Reserved.</p>
+        Copyright @ AR Shakir 2022. All Rights Reserved.
       </div>
     </div>
   );
